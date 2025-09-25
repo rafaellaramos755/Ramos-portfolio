08def2712c3e41d00564141164d04a7ebd768be3
rafaellaramos755 <rafaellamaeramos@gmail.com>
2025-09-25 20:22:57 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 
+const navLinks = document.querySelectorAll('nav a');
+const sections = document.querySelectorAll('main .section');
+
+
+document.querySelector('#about').classList.add('active');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const targetID = this.getAttribute('href').substring(1);
+
+ 
+    sections.forEach(section => section.classList.remove('active'));
+
+   
+    document.getElementById(targetID).classList.add('active');
+  });
+});
